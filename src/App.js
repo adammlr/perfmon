53812d81c0d813de6f96a450f5d7b8bbcbbf25a5
Adam Miller <adam.miller@csgi.com>
2021-09-24 10:12:04 -0500

    elastic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link
+} from "react-router-dom";
+import { ApmRoute as Route } from '@elastic/apm-rum-react';
+
+import About from './About';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { init as initApm } from '@elastic/apm-rum';
+
+initApm({
+  // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
+  serviceName: 'CRA',
+
+  // Set custom APM Server URL (default: http://localhost:8200)
+  serverUrl:
+    'https://97a885be442f446c8b446b64444fb9f4.apm.eastus2.azure.elastic-cloud.com:443',
+
+  // Set service version (required for sourcemap feature)
+  serviceVersion: ''
+});
 
 ReactDOM.render(
   <React.StrictMode>
